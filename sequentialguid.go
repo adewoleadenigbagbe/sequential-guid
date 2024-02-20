cecc2105428db9243fdbec06e207e70a2d958a3c
Adewole <wolecharles@gmail.com>
2024-02-20 11:11:21 +0100

    edit comment

--- sequentialguid.go
+++ sequentialguid.go
@@ -17,7 +17,6 @@ var Endianess binary.ByteOrder
 type SequentialGuid uuid.UUID
 
 // New creates a sequential guid that can be used as the primary key to solve the randomness of Globally Unique Identifier (GUID or UUID).
-//
 // By RFC 4122, the standard GUID is 16 bytes, the first 6 bytes is generated from UTC timestamp and the next 10 bytes are generated randomly,
 // the Endianess are the computer architecture are also considered
 //
